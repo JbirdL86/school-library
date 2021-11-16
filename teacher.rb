2e3a7320ac80e919ce62c558a82d3dfa288e3f3a
JbirdL86 <jgonzalez.wj@gmail.com>
2021-11-15 19:17:07 -0600

    Add specialization to Teacher class

--- teacher.rb
+++ teacher.rb
@@ -1,6 +1,11 @@
 require './person'
 
 class Teacher < Person
+  def initialize(id, age, specialization, name = 'Unknown', parent_permission = true)
+    super(id, age, name, parent_permission)
+    @specialization = specialization
+  end
+
   def can_use_services?
     true
   end
