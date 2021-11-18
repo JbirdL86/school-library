65fac08d2b1b051fdcba19131271f0183289894d
JbirdL86 <jgonzalez.wj@gmail.com>
2021-11-18 16:34:22 -0600

    Fix addint classroom to a student and adding student to classroom

--- classroom.rb
+++ classroom.rb
@@ -10,6 +10,6 @@ class Classroom
 
   def add_student(student)
     @students.push(student)
-    student.classroom = self
+    student.add_classroom(self)
   end
 end
--- student.rb
+++ student.rb
@@ -9,7 +9,7 @@ class Student < Person
     @classroom = classroom
   end
 
-  def classroom(classroom)
+  def add_classroom(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
